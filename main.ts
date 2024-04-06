82fd54453f0e51d12a7e01b57a5f7fee1deb97fd
alikashi11 <165997080+alikashi11@users.noreply.github.com>
2024-04-06 16:07:55 +0500

    push my project to github

---
+++ main.ts
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+
+let myBalnace = 10000;
+
+let myPIN = 1111;
+let PinAnswer = await inquirer.prompt([
+    {
+        name: "Pin",
+        message: "enter your Pin",
+        type: "number",
+    }
+]);
+if (PinAnswer.Pin === myPIN) {
+    console.log("Correct Pin Code!!!");
+
+    let operationAns = await inquirer.prompt(
+        
+    [
+        {
+            name: "operation",
+            message: "please select opition",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }
+    ]
+);
+console.log(operationAns);
+if (operationAns.operation  === "withdraw"){
+    let amountAns = await inquirer.prompt(
+   [
+      {
+     name:"amount",
+     message:"enter your amount",
+     type:"number"
+      }
+      
+   ]  
+ );
+
+myBalnace -= amountAns.amount;
+
+console.log("your remaning balance is: " + myBalnace)
+}else if  (operationAns.operation  === "check balance"){
+    console.log("your balance is: " + myBalnace)
+}
+
+
+}
+
+
+
+
+
+else {
+    console.log("Incorrect pin number");
+}
